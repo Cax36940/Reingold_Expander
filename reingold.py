d27bc27be0df7f599caafb94d638d3e6eced611b
ORDRONNEAU Camille <Camille.Ordronneau@bordeaux-inp.fr>
2024-04-28 16:03:56 +0200

    Construction de H

--- reingold.py
+++ reingold.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 ##### Useful Functions #####
 
 def graphPower(G, n):
@@ -51,23 +51,52 @@ def maxPower(N, D):
 
         return : l : int
     """ 
+    return int(np.ceil(-np.log2(-np.log2(1-1/(D*N*N)))))
 
 ##### USTCON #####
 
 # Notation N or D instead of [|1, N|] or [|1, D|]
 
+D = 67 * 67 # q = 67
+#TODO peut-êre utiliser un cycle
 def rotH(vertex, edge):
     """
-        Rotation map of (D^16, D^2, 1/2) graph H
-
-        global const D
+        Rotation map of (D^16, D, 1/2) graph H
 
         vertex : int[16]    in D^16
-        edge : int[2]       in D^2
+        edge : int       in D
 
         return : (vertex', edge') : (int[16], int[2])
     """
-    return 0
+
+    q = 67
+    d = 31
+    x = edge // q
+    y = edge % q
+
+    # convert vertex form int[16] in D^16 to int[32] in q^32
+    a = [0 for i in range(d + 1)]
+
+    for i in range(32):
+        if i % 2 :
+            a[i] = vertex[i//2] % q
+        else :
+            a[i] = vertex[i//2] // q
+        
+    b = [(y * (x**i))%q for i in range(d + 1)]
+
+    c = [(a[i] + b[i])%q for i in range(d + 1)]
+
+
+    invy = -y % q
+
+    new_vertex = [0 for i in range(16)]
+
+    for i in range(16):
+        new_vertex[i] = c[2*i] * 67 + c[2*i+1]
+
+
+    return [new_vertex, x*67 + ((-y)%67)]
 
 
 def rotGreg(vertex, edge):
@@ -85,7 +114,8 @@ def rotGreg(vertex, edge):
     """
     return 0
 
-
+#TODO Test une itération de Reingold avec une H = 3-cycle
+#       on pourrait faire le test avec plusieurs graphes de degré 3
 def rotGexp(vertex, edge):
     """
         Rotation map of (N² x (D^16)^L, D^16, 1/2) expander graph
@@ -155,7 +185,11 @@ def piPerm(x):
 
         return : y : int
     """
-
+    if x == 1 :
+        return 2
+    if x == 2 :
+        return 1
+    return x
 
 
 def Ae2p(vertex, edge, N):
@@ -165,40 +199,114 @@ def Ae2p(vertex, edge, N):
         global const D
 
         dependancies :
+            maxPower
             rotH
             piPerm
 
-        vertex : [(int, int), int[L][16]]   in N² x (D^16)^L        #(int, int) is not necessary
+        vertex : int[L][16]
         edge : int[16]                      in D^16
         N : int
 
         (computations are similar to the ones of rotGexp)
 
         return : path : int[][16]     in (D^16)*
+                 next_vertex : int[L][16]
 
     """
-    return 0
+    path = []
+    global D
+    l = maxPower(N, D)
+    l = 6
+    # Allocate variables
+    a = [[0 for i in range(16)] for k in range(l + 1)]
+    I = 0
+    j = [0 for i in range(l + 1)]
+
+    # Initialise variables
+    for i in range(l):
+        for k in range(16):
+            a[i][k] = vertex[i][k]
+    for k in range(16):  
+        a[l][k] = edge[k]
+
+    I = l
+
+    for i in range(1, l + 1):
+        j[i] = 1
+    
+
+    while 1 :
+
+        # Step 1
+        # print(a)
+        new_a = rotH(a[I-1],a[I][j[I]-1])
+        a[I-1] = new_a[0]
+        a[I][j[I]-1] = new_a[1]
+
+        # Step 2
+        if j[I]%2:
+            if I == 1 :
+                if a[0][:-2] == [1 for i in range(15)]:
+                    if a[0][15] <= 3 :
+                        print((a[0][15], piPerm(a[0][15])))
+                        a[0] = piPerm(a[0])
+                j[I] += 1
+
+        # Step 3
+            elif I > 1 :
+                j[I-1] = 1
+                I -= 1
+         
+        # Step 4
+        elif j[I] == 16 :
+            #reverse a[I]
+            for i in range(8):
+                tmp = a[I][i]
+                a[I][i] = a[I][15 - i]
+                a[I][15 - i] = tmp
+        
+        # Step 5
+            if I == l :
+                break
+
+        # Step 6
+            elif I < l :
+                j[I + 1] += 1
+                I += 1
+        else:
+            j[I] += 1
+
+    return path, vertex
 
 
-def Asrch(path, N): #not really what was defined in the paper
+
+
+
+def Asrch(path, N, L): #not really what was defined in the paper
     """
-        Compute path in a graph with pi-permutation, taken while walking on L an edges in its expander transformation
-
+        Compute path in a graph with pi-permutation, taken while walking on Delta an edges in its expander transformation
 
         dependancies :
+            piPerm
             Ae2p
 
-
-        path : path : int[L][16]     in (D^16)^L
+        path : path : int[L][16]     in (D^16)^Delta
         N : int
 
         calls Ae2p for each edge of the path
 
-
         return : path : int[][16]     in (D^16)*
     """
+    entire_path = []
+    vertex = [0 for i in range(L)]
 
+    for edge in path :
+        tmp_path, vertex = Ae2p(vertex, edge, N)
+        entire_path = entire_path + list(tmp_path)
+    
 
+    entire_path = entire_path + [piPerm(x) for x in entire_path].reverse()
+    return entire_path
 
 
 def UTS(N, Dmax):
@@ -211,11 +319,19 @@ def UTS(N, Dmax):
         dependancies :
             Asrch
 
-        calls Asrch on every path of length L of edges in D^16
+        calls Asrch on every path of length Delta of edges in D^16
 
         return : UTS : int[][16] in D^16
     """
 
+    Delta = pathLength(N, Dmax)
+    L = maxPower(N, Dmax)
+    uts = []
+    for i in range((D**16)**Delta):
+        path = [(i//((D**16)**k))%D**16 for k in range(L)]
+        uts = uts + Asrch(path, N, L)
+
+    return uts
 
 
 def toXS(TS):
@@ -228,6 +344,10 @@ def toXS(TS):
 
         return : XS : int[]
     """
+
+
+
+
     return 0
 
 
@@ -242,6 +362,7 @@ def UXS(N, Dmax):
 
         dependancies :
             UTS
+            
             toXS
 
         calls UTS
@@ -249,3 +370,7 @@ def UXS(N, Dmax):
 
         return : UXS : int[] in Dmax*
     """
+    return toXS(UTS(N, Dmax))
+#TODO faire un graph, sa transformation en TS en XS
+# print(maxPower(8, 67*67))
+Ae2p([[1 for i in range(16)] for k in range(6)], [1 for i in range(16)], 8)
